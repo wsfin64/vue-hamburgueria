f95ea76f7bce5d28459cd163dd25c3e655027b8c
Wellington Silva <wsfin64@gmail.com>
2023-07-08 23:08:43 -0300

    inputs e data binding

--- 1_instalacao_vue/src/app.js
+++ 1_instalacao_vue/src/app.js
@@ -1,9 +1,26 @@
 const MyNameApp = {
     data(){
         return {
-            name: "Wellington",
-            age: 30
+            name: "",
+            age: 30,
+            input_name: "",
+            image: "",
+            input_image: ""
         }
+    },
+    methods: {
+        submitForm(e) {
+            e.preventDefault();
+
+            console.log(this.input_name);
+            this.name = this.input_name;
+        },
+        submitImage(ev) {
+            ev.preventDefault();
+
+            this.image = this.input_image;
+        }
+        
     }
 }
 
